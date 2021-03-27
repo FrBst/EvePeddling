bf62c36507bb0daca1b864d3543151329eb1c2cf
MK <frbeast@outlook.com>
2021-03-27 13:18:08 +0300

    + Fixed build errors. + Fixed ApiException when fetching order lists. + Output of the results to console (yes, it can be called a proper program now)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,18 +4,17 @@ import net.troja.eve.esi.api.UniverseApi;
 import net.troja.eve.esi.model.*;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
 
     private static List<Integer> resolveSystemNames(List<String> names) throws ApiException {
         UniverseApi universe = new UniverseApi();
-        List<UniverseIdsSystem> response;
-        response = universe.postUniverseIds(names, null, "tranquility", null).getSystems();
+        List<UniverseIdsSystem> response =
+                universe.postUniverseIds(names, null, "tranquility", null).getSystems();
 
-        List<Integer> ids = new ArrayList<Integer>();
-        for (UniverseIdsSystem system : response)
-            ids.add(system.getId());
-        return ids;
+        return  response != null ? response.stream().map(UniverseIdsSystem::getId).collect(Collectors.toList()) :
+                Collections.emptyList();
     }
 
     private static Set<Integer> getRegions(List<Integer> systems) throws ApiException {
@@ -40,39 +39,51 @@ public class Main {
         MarketApi market = new MarketApi();
         List<MarketOrdersResponse> marketOrdersResponses;
         int page = 1;
-        List<MarketOrdersResponse> appropriateOrders = new ArrayList<MarketOrdersResponse>();
+        List<MarketOrdersResponse> appropriateOrders = new ArrayList<>();
         for (int region : regions) {
-            do {
-                marketOrdersResponses = market.getMarketsRegionIdOrders(order_type,
-                        region, "tranquility", null, page, null);
-                for (MarketOrdersResponse response : marketOrdersResponses) {
-                    for (int system : systems) {
-                        if (response.getSystemId() == system)
-                            appropriateOrders.add(response);
+            try {
+                do {
+                    marketOrdersResponses = market.getMarketsRegionIdOrders(order_type,
+                            region, "tranquility", null, page, null);
+                    for (MarketOrdersResponse response : marketOrdersResponses) {
+                        for (int system : systems) {
+                            if (response.getSystemId() == system)
+                                appropriateOrders.add(response);
+                        }
                     }
-                }
-                page++;
-            } while (marketOrdersResponses.size() > 0);
+                    page++;
+                } while (marketOrdersResponses.size() > 0);
+            } catch (ApiException e) { }
+            page = 1;
         }
 
-        if (order_type == "buy")
-            Collections.sort(appropriateOrders, new PriceAsc());
+        if (order_type.equals("buy"))
+            appropriateOrders.sort(new PriceAsc());
         else
-            Collections.sort(appropriateOrders, new PriceDesc());
-        Collections.sort(appropriateOrders, new TypeAsc());
+            appropriateOrders.sort(new PriceDesc());
+        appropriateOrders.sort(new TypeAsc());
         return appropriateOrders;
     }
 
     private static List<Deal> solve(List<Integer> startSystems, List<Integer> endSystems, int capital, double storage) throws ApiException {
-        List<MarketOrdersResponse> toBuy = getOrders(startSystems, "sell");
-        List<MarketOrdersResponse> toSell = getOrders(endSystems, "buy");
-        List<Deal> deals = new ArrayList<Deal>();
-
+        List<MarketOrdersResponse> toBuy;
+        List<MarketOrdersResponse> toSell;
+        List<Deal> deals;
+//        while (true) {
+//            try {
+                toBuy = getOrders(startSystems, "sell");
+                toSell = getOrders(endSystems, "buy");
+                deals = new ArrayList<>();
+//                break;
+//            } catch (ApiException e) {
+//                System.out.println("API exception: " + e.getMessage());
+//            }
+//        }
         int bo = 0;
         int so = 0;
         int buyAvailable = toBuy.get(bo).getVolumeRemain();
         int sellAvailable = toSell.get(so).getVolumeRemain();
-        int batch = 0;
+        int batch;
         while (bo < toBuy.size() && so < toSell.size()) {
             if (toSell.get(so).getTypeId() < toBuy.get(bo).getTypeId()) {
                 so++;
@@ -99,10 +110,7 @@ public class Main {
                 continue;
             }
 
-            if (sellAvailable < buyAvailable)
-                batch = sellAvailable;
-            else
-                batch = buyAvailable;
+            batch = Math.min(sellAvailable, buyAvailable);
             sellAvailable -= batch;
             buyAvailable -= batch;
             deals.add(new Deal(toBuy.get(bo).getTypeId(),
@@ -113,34 +121,32 @@ public class Main {
                     toSell.get(so).getSystemId()));
         }
 
-        Collections.sort(deals, new MarginDesc());
+        deals.sort(new MarginDesc());
         return deals;
     }
 
     public static List<String> interpret (List<Deal> deals) throws ApiException {
-        List<String> ans = new LinkedList<String>();
+        List<String> ans = new LinkedList<>();
 
-        Set<Integer> temp = new HashSet<Integer>();
+        Set<Integer> temp = new HashSet<>();
         for (Deal deal : deals) {
             temp.add(deal.getBuySystem());
             temp.add(deal.getSellSystem());
             temp.add(deal.getType());
         }
 
-        List<Integer> ids = new ArrayList<Integer>();
-        for (int id : temp)
-            ids.add(id);
+        List<Integer> ids = new ArrayList<>(temp);
         UniverseApi universe = new UniverseApi();
         List<UniverseNamesResponse> response;
         response = universe.postUniverseNames( ids, "tranquility");
 
-        Map<Integer, String> names = new HashMap<Integer, String>();
+        Map<Integer, String> names = new HashMap<>();
         for (int i = 0; i < ids.size(); i++) {
             names.put(response.get(i).getId(), response.get(i).getName());
         }
 
         for (Deal deal : deals) {
-            String line = new String("");
+            String line = "";
             line += names.get(deal.getBuySystem()) + " buy " + deal.getCount() + "x " + names.get(deal.getType())
                     + " for " + deal.getBuyPrice() + " ISK, " + names.get(deal.getSellSystem()) + " sell for "
                     + deal.getSellPrice() + " ISK; Margin " + deal.getMargin();
@@ -150,7 +156,8 @@ public class Main {
         return ans;
     }
 
-    public static void main(String args[]) throws ApiException {
+    public static void main(String[] args) throws ApiException {
+        System.out.println("Executing...");
 //        UniverseApi universe = new UniverseApi();
 //        List<UniverseNamesResponse> response;
 //        List<Integer> ids = new ArrayList<Integer>();
@@ -158,7 +165,9 @@ public class Main {
 //        response = universe.postUniverseNames( ids, "tranquility");
 
         List<Deal> deals = solve(resolveSystemNames(Arrays.asList("Amarr", "Jita")), resolveSystemNames(Arrays.asList("Pasha", "Tash-Murkon Prime")), 0, 0);
-        List<String> huy = interpret(deals);
-        String huy2;
+        List<String> results = interpret(deals);
+        for (String s : results) {
+            System.out.println(s);
+        }
     }
 }
